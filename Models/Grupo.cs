8547bfcabe06acf3eb32ad247d7e56ce2dbb3ed5
Android Studio <devandroid@ort>
2026-04-23 16:00:31 -0300

    CASI

--- Models/Grupo.cs
+++ Models/Grupo.cs
@@ -17,8 +17,8 @@ namespace TP01_ALFIE_FAINSCHTEIN.Models
     public void CargaDatosManual(){
 
 
- Integrante luca = new Integrante(50233233, "Luca Fainschtein", new DateTime(2000,10,01), "luca.jpg",  new List<string>{"Mamá: Marta Einmas", "papá: Martin Fainschtein "},  new List<string>{"Jugar a la play", "Ver la tele"});
- Integrante liam = new Integrante(12345678, "Liam   Galanternik",  new DateTime(2010,01,05), "liam.jpg",  new List<string>{"Papá: Nicolas Galanternik", "Mamá: Romina Lopez "},  new List<string>{"Viajar", "Leer"} );
+ Integrante luca = new Integrante(50233233, "Luca Fainschtein", new DateTime(2000,10,01), "luca.jpg",  new List<string>{"Mamá: Marta Einmas", "Papá: Martin Fainschtein "},  new List<string>{"Jugar a la play", "Ver la tele"});
+ Integrante liam = new Integrante(12345678, "Liam   Galanternik",  new DateTime(2010,01,05), "liam.jpg",  new List<string>{ "Mamá: Romina Lopez ", "Papá: Nicolas Galanternik"},  new List<string>{"Viajar", "Leer"} );
  Integrante martin = new Integrante(98765432, "Martin Torres", new DateTime(2009,09,04), "martin.jpg",  new List<string>{"Mamá: Sofia Cohen", "Papá: Lucas Torres "},  new List<string>{"Aprender idiomas", "Yoga"} );
  Integrante matias = new Integrante(12345365, "Matias Lopez", new DateTime(2005,07,07), "matias.gif",  new List<string>{"Mamá: Priscila Levy", "Papá: Nicolas Lopez"},  new List<string>{"Programar", "Estudiar"} );
 
